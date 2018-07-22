6f891a54402e8b7311643a3edc15a977bef366a8
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2018-07-22 23:45:09 +0700

    Fixed ipfs building

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,6 @@
 const { rewireEmotion } = require("react-app-rewire-emotion");
 const webpack = require("webpack");
+const { getLoader, loaderNameMatches } = require("react-app-rewired");
 
 module.exports = function override(config, env) {
   config = rewireEmotion(config, env);
@@ -8,6 +9,16 @@ module.exports = function override(config, env) {
       "process.env.VERSION": JSON.stringify(require("./package.json").version)
     })
   ]);
+
+  const babelLoader = getLoader(config.module.rules, rule =>
+    loaderNameMatches(rule, "babel-loader")
+  );
+
+  config.module.rules.push({
+    test: /node_modules\/(ipfs-.*?|cids|multihashes|is-ipfs|dag-link|ipld-.*?|multiaddr|class-is|multihashing-async|borc|peer-id|libp2p-crypto|libp2p-crypto-.*?|multibase|multicodec|peer-info)\/(.*?)\.js$/,
+    loader: babelLoader.loader,
+    options: babelLoader.options
+  });
 
   return config;
 };
