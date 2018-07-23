b3ebd2edf2387ea4f9f81b282789813ce43948e9
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2018-07-24 00:54:39 +0700

    Stop when : is missing from url

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-if (window.location.hash) {
-	import('./Downloader').then((Component) => {
+function init() {
+	if (window.location.hash) {
 		let [hash, key] = window.location.hash.substr(1).split(':');
-		ReactDOM.render(
-			<Component.default hash={hash} encryptKey={key} />,
-			document.getElementById('root')
-		);
-	});
-} else {
-	import('./Uploader').then((Component) => {
-		ReactDOM.render(<Component.default />, document.getElementById('root'));
-	});
+
+		if (!key) {
+			document.getElementById('root').textContent =
+				'Key not specified. Did you miss the colon?';
+			return;
+		}
+
+		import('./Downloader').then((Component) => {
+			ReactDOM.render(
+				<Component.default hash={hash} encryptKey={key} />,
+				document.getElementById('root')
+			);
+		});
+	} else {
+		import('./Uploader').then((Component) => {
+			ReactDOM.render(
+				<Component.default />,
+				document.getElementById('root')
+			);
+		});
+	}
 }
+
+init();
